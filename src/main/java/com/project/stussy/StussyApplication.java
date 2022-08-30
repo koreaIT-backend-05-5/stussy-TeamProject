23a0ec5260f6a0453e4d5bd29e8066fe6b4a02ef
DESKTOP-UVHELJ7\eastzi <zero7038@naver.com>
2022-08-30 20:14:18 +0900

    stussy project 기초세팅

---
+++ src/main/java/com/project/stussy/StussyApplication.java
@@ -0,0 +1,13 @@
+package com.project.stussy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyApplication.class, args);
+	}
+
+}
