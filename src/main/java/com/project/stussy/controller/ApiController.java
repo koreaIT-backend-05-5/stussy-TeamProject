b79b213721971066d38466318f0285513afe8336
DESKTOP-UVHELJ7\eastzi <zero7038@naver.com>
2022-09-06 17:26:46 +0900

    main에 thymeleaf로 header, footer 추가

--- src/main/java/com/project/stussy/controller/ApiController.java
+++ src/main/java/com/project/stussy/controller/ApiController.java
@@ -2,12 +2,14 @@ package com.project.stussy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class ApiController {
 
-	@GetMapping("/auth/signin")
+	@GetMapping("/main")
 	public String loadmain() {
-		return "/auth/signin";
+		return "main/main";
 	}
 }
