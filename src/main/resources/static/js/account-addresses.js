32859d16b8c59518311c35841ba5da84427e1837
ix00xi <izx43@naver.com>
2022-09-07 14:57:47 +0900

    checkouts(주문-유저주소),checkouts-card(주문-카드확인) 페이지 작성

--- src/main/resources/static/js/account-addresses.js
+++ src/main/resources/static/js/account-addresses.js
@@ -3,7 +3,7 @@ const updateAddressesArea = document.querySelector(".update-addresses-area");
 const addressesShowHideInfo = document.querySelector(".addresses-show-hide-info");
 const addAddressesBtn = document.querySelector(".add-addresses-btn");
 const addressesListTxt = document.querySelector(".addresses-list-txt");
-
+const createAddressesArea = document.querySelector(".create-addresses-area");
 
 updateBtn.onclick = () => {
     if(updateAddressesArea.style.display != 'none') {
@@ -14,6 +14,18 @@ updateBtn.onclick = () => {
         updateAddressesArea.style.display = 'none'
     }
      
+}
+
+addAddressesBtn.onclick = () => {
+    if(updateAddressesArea.style.display == 'block') {
+        updateAddressesArea.style.display = 'none'
+        updateAddressesArea.style.height = '427px'
+        createAddressesArea.style.display = 'block'
+    } else {
+        addressesShowHideInfo.style.display = 'none';
+        addAddressesBtn.style.height = "20px"
+        createAddressesArea.style.display = 'block'
+    }
 }
 
 
