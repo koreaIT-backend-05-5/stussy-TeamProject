ea65f6733330b17a0c956e2685d330f626c3b108
zxcxv15 <zxcxv15@daum.net>
2022-10-18 20:11:37 +0900

    상품 수정하기 진행중

--- src/main/resources/static/js/product/product-list.js
+++ src/main/resources/static/js/product/product-list.js
@@ -1,29 +1,23 @@
-const categorySelectInput = document.querySelector(".category-select .product-input");
-const searchInput = document.querySelector(".product-search .product-input");
-const searchButton = document.querySelector(".search-button"); 
-
-let productDataList = null;
 
 let nowPage = 1;
 
-getList(nowPage);
+load(nowPage);
 
 //db 데이터 불러오는 ajax
-function getList(nowPage) {
+function load(nowPage) {
 	
 	$.ajax({
 		async: false,
 		type: "get",
 		url: "/api/v1/manager/product-list/" + nowPage,
+		
 		dataType: "json",
 		success: (response) => {
 			if(response.data[0] != null) {
-				addProducts(response.data);
+				getList(response.data);
 				getPageNumbers(response.data[0].totalProductCount);
-				productDataList = response.data;
-                addProducts(productDataList);
 			}else{
-				addProducts(new Array());
+				getList(new Array());
 				getPageNumbers(0);
 			}
 		},
@@ -34,8 +28,9 @@ function getList(nowPage) {
 	
 }
 
+
 //상품 리스트 불러오기
-function addProducts(list){
+function getList(list){
 	const tbody = document.querySelector("tbody");
 	tbody.innerHTML = "";
 	
@@ -50,94 +45,11 @@ function addProducts(list){
                 <td><button type="button" class="list-button detail-button"><i class="fa-regular fa-file-lines"></i></button></td>
             	<td><button type="button" class="list-button delete-button"><i class="fa-regular fa-trash-can"></i></button></td>             
             </tr>
-          	<tr class="product-detail detail-invisible">
-            
-        	</tr>
 		`;
 	});
 	
-	const detailButtons = document.querySelectorAll(".detail-button");
-    const productDetails = document.querySelectorAll(".product-detail");
-
-    detailButtons.forEach((detailButton, index) => {
-        detailButton.onclick = () => {
-
-            if(productDetails[index].classList.contains("detail-invisible")) {
-                let changeRequestFlag = false;
-                let changeFlag = false;
-                
-                productDetails.forEach((productDetail, index2) => {
-                    if(!productDetail.classList.contains("detail-invisible") && index2 != index){
-                        changeRequestFlag = true;
-                        changeFlag = confirm("수정을 취소하시겠습니까?");
-                        if(changeFlag) {
-                            productDetail.classList.add("detail-invisible");
-                            productDetail.innerHTML = "";
-                            getProductDetail(productDetails[index], index);
-                            productDetails[index].classList.remove("detail-invisible");
-                        }
-                    }else {
-                        if(changeRequestFlag && changeFlag) {
-                            getProductDetail(productDetails[index], index);
-                            productDetails[index].classList.remove("detail-invisible");
-                        }else if(!changeRequestFlag) {
-                            getProductDetail(productDetails[index], index);
-                            productDetails[index].classList.remove("detail-invisible");
-                        }
-                    }
-                });
-                
-            }else{
-                if(confirm("수정을 취소하시겠습니까?")){
-                    productDetails[index].classList.add("detail-invisible");
-                    productDetails[index].innerHTML = "";
-                }
-            }            
-        }
-    });
-}
-
-function getProductDetail(productDetail, index) {
-    productDetail.innerHTML = `
-    <td colspan="8">
-        <table class="product-info">
-            <tr>
-                <td><input type="text" class="product-input" value="${productDataList[index].prodictPrice}" placeholder="가격"></td>
-                <td><input type="text" class="product-input" value="${productDataList[index].productSize}" placeholder="사이즈"></td>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <textarea class="product-input" placeholder="설명">${productDataList[index].productInfo}</textarea>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <form enctype="multipart/form-data">
-                        <div class="product-img-inputs">
-                            <label>상품 이미지</label>
-                            <button type="button" class="add-button">추가</button>
-                            <input type="file" class="file-input product-invisible" name="file" multiple>
-                        </div>
-                    </form>
-                    <div class="product-images">
-
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <button type="button" class="black-button update-button">수정하기</button>
-                </td>
-            </tr>
-        </table>
-    </td>
-    `;
-    
-}
-
-	/*------------------------------------------------------------*/
 	//상품 삭제
-	const deleteBtns = document.querySelectorAll(".delete");
+	const deleteBtns = document.querySelectorAll(".delete-button");
 	
 		for(let i = 0; i < deleteBtns.length; i++) {
 		deleteBtns[i].onclick = () => {
@@ -153,20 +65,22 @@ function getProductDetail(productDetail, index) {
 				},
 				error: (error) => {
 					console.log(error);
-			}	
-			
+			}
 		});
 	}
 }
-	
+
 	//상품 수정 페이지로 넘어가기
-	const modifyBtns = document.querySelectorAll(".product-row .modify");	
+	const modifyBtns = document.querySelectorAll(".detail-button");	
 	
 	for(let i = 0; i < modifyBtns.length; i++) {
 		modifyBtns[i].onclick = () => {
 			location.href = `/manager/product-modify/${list[i].productCode}`
 		}
 	}
+}	
+	
+	
 
 	
 function getPageNumbers(totalProductCount) {
@@ -209,16 +123,15 @@ function getPageNumbers(totalProductCount) {
 	
 	
 	
-// 다음 페이지로 넘기기
-const pageNumberButtons = document.querySelectorAll(".page-button");
-
-	pageNumberButtons.forEach(button => {
-		if(button.textContent != "<" && button.textContent != ">"){
-			button.onclick = () => {
-				nowPage = button.textContent;
-				load(nowPage);
+	// 다음 페이지로 넘기기
+	const pageNumberButtons = document.querySelectorAll(".page-button");
+	
+		pageNumberButtons.forEach(button => {
+			if(button.textContent != "<" && button.textContent != ">"){
+				button.onclick = () => {
+					nowPage = button.textContent;
+					load(nowPage);
+				}
 			}
-		}
-	});
-}
-
+		});
+	}
--- src/main/resources/static/js/product/product-modify.js
+++ src/main/resources/static/js/product/product-modify.js
@@ -1,11 +1,14 @@
 const productCode = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+const fileInput = document.querySelector(".file-input");
+
+let fileObject = null;
+
 
 getProductDetailData();
 console.log(productCode);
 
 //데이터를 가져오는 ajax
 function getProductDetailData () {
-	
 		
        $.ajax({
 		async: false,
@@ -20,128 +23,166 @@ function getProductDetailData () {
 			console.log(error);
 		}		
 	});
+}
        
 //수정 페이지로 넘어갈 때 value 값도 같이 넘어감
 function getModify(productDetailData){
 	
-	const productDetailDataInput = document.querySelector("table");
+	const productDetailDataInput = document.querySelector("section");
 	productDetailDataInput.innerHTML = "";
 	
-	console.log(productDetailDataInput);
+	console.log(productDetailData);
+	console.log(productDetailData.fileName)
 	
-		productDetailDataInput.innerHTML = `
-        <tr>
-            <td>카테고리</td>
-            <td>
-                <select class="product_category" >
+		productDetailDataInput.innerHTML += `
+    		 <section>
+              	 <select class="product_category product-input">
                     <option value="0">ALL</option>
-                    <option value="1">SHIRT</option>
-                    <option value="2">PANTS</option>
-                    <option value="3">CAP</option>
-                    <option value="4">ACCESSORY</option>
+                    <option value="1">TOPS & SHIRTS</option>
+                    <option value="2">BOTTOMS</option>
+                    <option value="3">HEADWEAR</option>
+                    <option value="4">ACCESSORIES</option>
                 </select>
-            </td>
-        </tr>
 
-        <tr>
-            <td>사이즈</td>
-            <td>
-                <select class="product_size">
-                    <option value="S">S</option>
-                    <option value="M">M</option>
-                    <option value="L">L</option>
-                    <option value="XL">XL</option>
-                    <option value="XXL">XXL</option>
-                </select>
-            </td>
-        </tr>
-        
-    <tr>
-        <td>상품명</td>
-        <td><input value="${productDetailData.productName}" type="text" name="productName" id="productName"></td>
-    </tr>
-    
-    <tr>
-        <td>가격</td>
-        <td><input value="${productDetailData.productPrice}" type="text" name="productPrice" id="productPrice"></td>
-    </tr>
-    
-    <tr>
-        <td>상품설명</td>
-        <td>
-            <textarea id="ir1" name="productExplanation">${productDetailData.productExplanation}</textarea>
-        </td>
-    </tr>
-    
-    <tr>
-        <td>상품이미지</td>
-		<td>
-			<input type="file"name="file" multiple="multiple" accept=".jpg, .jpeg, .png, .gif">
-			${productDetailData.fileName}
-		</td>
-    </tr>
-    
-    <tr>
-        <td colspan="2" align="center">
-       	 	<button type="button" class="product-button submit" >수정</button>
-        	<button type="button" class="product-button cancel" >취소</button>
-        </td>
-    </tr>
+                <input type="text" class="product-input" value="${productDetailData.productName}" placeholder="상품명을 입력하세요">
+                <input type="text" class="product-input" value="${productDetailData.productPrice}" placeholder="가격을 입력하세요">
+                
+                <div class="product-option">
+                    <select class="product_size product-input">
+                        <option value="S">S</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+                        <option value="XXL">XXL</option>
+                    </select>
+                </div>
+
+                <textarea class="product-input" placeholder="설명">${productDetailData.productInfo}</textarea>
+               	<form enctype="multipart/form-data">
+                <div class="product-img-inputs">
+                    <label>상품 이미지</label>
+                    <button type="button" class="modify-button">수정</button>
+                    <input type="file" class="file-input product-invisible" name="file">
+                </div>
+        		</form>
+        	
+                <div class="product-images">
+                  <div class="img-box">
+	                <i class="fa-solid fa-xmark"></i>
+	                <img src="/image/product/${productDetailData.fileName}">
+	            </div>
+                    
+                </div>
+                
+                
+                <button type="button" class="submit-button product-button black-button">수정하기</button>
+            </section>
 		`;
+
+	//수정 버튼
+	const modifyAddButton = document.querySelector(".modify-button");
+	const fileInput = document.querySelector(".file-input");
+	
+	modifyAddButton.onclick = () => {
+    	fileInput.click();
+	}	
+	
+	fileInput.onchange = () => {
+    const formData = new FormData(document.querySelector("form"));
+    let changeFlge = false;
+
+    formData.forEach((value) => {
+        if(value.size != 0) {
+            fileObject = value;
+            changeFlge = true;
+        }
+    });
+    
+        if(changeFlge){
+        getImagePreview(fileObject);
+        fileInput.value = null;
+	    }
+	}
+	
+	function getImagePreview(fileObject) {
+    const productImages = document.querySelector(".product-images");
+
+    productImages.innerHTML = "";
+    
+    if(fileObject != null) {
+		modifyAddButton.disabled = true;
+		modifyAddButton.classList.toggle("selected-image");
 		
-	let = submitButton = document.querySelector(".submit");
-	let = cancelButton = document.querySelector(".cancel");
+	}else {
+		modifyAddButton.disabled = false;
+		modifyAddButton.classList.toggle("selected-image");
+		
+		}
+    }   
+    const reader = new FileReader();
+    
+ 
+	const deleteButton = document.querySelector(".fa-xmark");
+    
+    deleteButton.onclick = () => {
+        if(confirm("상품 이미지를 지우시겠습니까?")) {
+            fileObject = null;
+            getImagePreview(fileObject);
+    	}
+	};
+}
+        
+    if(fileObject != null) {
+    	reader.readAsDataURL(fileObject);
+	}
 	
-	//select value 값 들고 가기
-	const categorySelect = document.querySelectorAll(".product_category option");
-	const sizeSelect = document.querySelectorAll(".product_size option");
+
+
 	
-	categorySelect.forEach(option => {
-		if(option.value == productDetailData.productCategory) {
-			option.setAttribute("selected", true);
-		};
-	})
 	
-	sizeSelect.forEach(option => {
-		if(option.value == productDetailData.productSize) {
-			option.setAttribute("selected", true);
-		};
-	})
+//리스트에서 select value 값 들고 가기
+const categorySelect = document.querySelectorAll(".product_category option");
+const sizeSelect = document.querySelectorAll(".product_size option");
+
+categorySelect.forEach(option => {
+	if(option.value == productDetailData.productCategory) {
+		option.setAttribute("selected", true);
+	};
+})
+
+sizeSelect.forEach(option => {
+	if(option.value == productDetailData.productSize) {
+		option.setAttribute("selected", true);
+	};
+})
 	
 
 	
 //수정 버튼 눌렸을 때
 
-submitButton.onclick = () => {
-	
-	let productNameInput = document.querySelector("#productName");
-	let productPriceInput = document.querySelector("#productPrice");
-	let textArea = document.querySelector("#ir1");
-	
-	let productCategorySelect = document.querySelector(".product_category");
-	let productSizeSelect = document.querySelector(".product_size");
+let submitButton = document.querySelector(".submit-button");
 
+	submitButton.onclick = () => {
 	
-	let selectCategoryData = productCategorySelect.options[productCategorySelect.selectedIndex].value;	
-	let selectSizeData = productSizeSelect.options[productSizeSelect.selectedIndex].value;
+	let productInput = document.querySelectorAll(".product-input");
 	
 	
+	let formData = new FormData();
 	
-	let formData = new FormData(document.querySelector("form"));
-	/*
-		formData.append("productName",productNameInput.value);
-		formData.append("productPrice",productPriceInput.value);
-		formData.append("productExplanation",textArea.value);
-		*/
-		formData.append("categoryCode",selectCategoryData);
-		formData.append("productSize",selectSizeData);
+		formData.append("categoryCode",productInput[0].value);
+		formData.append("productName",productInput[1].value);
+		formData.append("productPrice",productInput[2].value);
+		formData.append("productSize",productInput[3].value);
+		formData.append("productInfo",productInput[4].value);
+		formData.append("file", fileObject);
+				
 		
 		formData.forEach((v, k) => {
 			console.log("key: " + k);
 			console.log("value: " + v);
 		});
 	
-       $.ajax({
+	   $.ajax({
 		async: false,
 		type: "put",
 		url: `/api/v1/manager/product-modify/${productCode}`,
@@ -158,19 +199,7 @@ submitButton.onclick = () => {
 		},
 		error: (error) => {
 			console.log(error);
+			
+				}		
+			});
 		}		
-	});
-       
-}
-
-//취소 버튼 눌렸을 때
-cancelButton.onclick = () => {
-	location.replace("/manager/product-list");
-		}
-	}
-}
-	
-
-
-	
-	
